b1476f0f474067bdbb5a9374138ef5c5cc443e97
Christian <cchiconato@gmail.com>
2017-05-25 22:28:45 -0300

    setInterval implementation

--- src/main/java/br/com/intro/Main.java
+++ src/main/java/br/com/intro/Main.java
@@ -1,5 +1,8 @@
 package br.com.intro;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +12,12 @@ import br.com.intro.utils.AppContext;
 public class Main {
 
 	public static void main(String[] args) {
+		new Timer().scheduleAtFixedRate(new TimerTask(){
+		    @Override
+		    public void run(){
+		       System.out.println("A Kiss every 5 seconds");
+		    }
+		},0,25000);
 		SpringApplication.run(AppContext.class, args);
 	}
 
